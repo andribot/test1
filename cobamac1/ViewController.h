8330e51526a95c7349355a5180d391a8490edb7b
Andri SALIM <andri_lim666@yahoo.com>
2014-08-19 15:44:38 +0700

    Initial Commit

---
+++ cobamac1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  cobamac1
+//
+//  Created by techdev on 8/19/14.
+//  Copyright (c) 2014 itdirectorate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ cobamac1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  cobamac1
+//
+//  Created by techdev on 8/19/14.
+//  Copyright (c) 2014 itdirectorate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
